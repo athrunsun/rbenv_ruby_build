80c39301b6528e67529004984d7a95dcc8373b9e
Bruce Sun <bruce.sun@tubemogul.com>
2017-07-10 16:39:58 +0800

    Fix uploading to bintray.

--- upload_to_bintray.sh
+++ upload_to_bintray.sh
@@ -2,14 +2,9 @@
 
 echo "*****************Start upload_to_bintray.sh*****************"
 
-if ! [ -z ${PYTHON_2_VERSION+x} ]; then
-    echo "Uploading python2 binary tarball to bintray..."
-    curl -v -u $BINTRAY_USER_NAME:$BINTRAY_API_KEY --upload-file $TRAVIS_BUILD_DIR/dist/$PYTHON_2_TARBALL_SAVE_NAME "https://api.bintray.com/content/athrunsun/pyenv_python_build/python2/stable/$PYTHON_2_TARBALL_SAVE_NAME?publish=1&override=1"
-fi
-
-if ! [ -z ${PYTHON_3_VERSION+x} ]; then
-    echo "Uploading python3 binary tarball to bintray..."
-    curl -v -u $BINTRAY_USER_NAME:$BINTRAY_API_KEY --upload-file $TRAVIS_BUILD_DIR/dist/$PYTHON_3_TARBALL_SAVE_NAME "https://api.bintray.com/content/athrunsun/pyenv_python_build/python3/stable/$PYTHON_3_TARBALL_SAVE_NAME?publish=1&override=1"
+if ! [ -z ${RUBY_2_VERSION+x} ]; then
+    echo "Uploading ruby2 binary tarball to bintray..."
+    curl -v -u $BINTRAY_USER_NAME:$BINTRAY_API_KEY --upload-file $TRAVIS_BUILD_DIR/dist/$RUBY_2_TARBALL_SAVE_NAME "https://api.bintray.com/content/athrunsun/rbenv_ruby_build/ruby2/stable/$RUBY_2_TARBALL_SAVE_NAME?publish=1&override=1"
 fi
 
 echo "-----------------Done upload_to_bintray.sh-----------------"
